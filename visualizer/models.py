467b27366d412efa30097f2528013171ef6ec7d7
Joshnovski <josh.morphew@hotmail.com>
2024-01-22 08:20:03 -0600

    Add algorithms class to models.py

--- visualizer/models.py
+++ visualizer/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Algorithms(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    code = models.TextField()
+
+    def __str__(self):
+        return self.name
