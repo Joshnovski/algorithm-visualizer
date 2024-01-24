675927e6cfb5e10de12200f92b986ab7801feff5
Joshnovski <josh.morphew@hotmail.com>
2024-01-24 15:56:14 -0600

    Add styling to topbar elements

--- frontend/src/components/Topbar.js
+++ frontend/src/components/Topbar.js
@@ -3,7 +3,19 @@ import React from 'react';
 const Topbar = () => {
     return (
         <nav class="topbar">
-            Topbar Component
+            <div class="topbar-left-container">
+                Algorithm Visualiser
+            </div>
+            <div class="topbar-right-container">
+                <div class="build-btn right-btn"><i class="fa-solid fa-wrench icon"></i>Build</div>
+                <div class="play-btn right-btn"><i class="fa-solid fa-play icon"></i>Play</div>
+                <div class="progress-bar-container">
+                    <div class="step-btn">{"<"}</div>
+                    <div class="progress-bar"></div>
+                    <div class="step-btn">{">"}</div>
+                </div>
+                <div class="speed-slider right-btn">Speed</div>
+            </div>
         </nav>
     );
 };
