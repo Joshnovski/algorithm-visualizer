b058692e00966c4c8179360cb5a5de1be9802228
Joshnovski <josh.morphew@hotmail.com>
2024-01-24 12:57:35 -0600

    Add code editor fix

--- frontend/src/components/CodePane.js
+++ frontend/src/components/CodePane.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import AceEditor from 'react-ace';
+import "ace-builds/src-noconflict/mode-python";
+import "ace-builds/src-noconflict/theme-tomorrow_night_eighties";
+import "ace-builds/src-noconflict/ext-language_tools";
 
 const CodePane = () => {
     return (
         <div class="code-pane">
-            Code Component
-            /* Ace Editor: for showing code */
+            <AceEditor 
+            mode="python" 
+            theme="tomorrow_night_eighties"
+            setOptions={{
+                enableSnippets: false,
+                showLineNumbers: true,
+                tabSize: 2,
+                }}
+                style={{ width: '100%', height: '100%' }} 
+            />
         </div>
     );
 };
