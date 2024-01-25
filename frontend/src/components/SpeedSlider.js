14aa3f504bfc984e7be3702b68d99449bec5e5b6
Joshnovski <josh.morphew@hotmail.com>
2024-01-25 15:40:05 -0600

    Add speed slider init

---
+++ frontend/src/components/SpeedSlider.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactSlider from 'react-slider';
+
+const SpeedSlider = () => {
+    return (
+        <div class="speed-slider right-btn">Speed</div>
+    );
+};
+
+export default SpeedSlider;
