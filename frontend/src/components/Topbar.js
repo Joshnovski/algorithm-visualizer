b09ac8fac2f10952dc87b882b8766c4104847974
Joshnovski <josh.morphew@hotmail.com>
2024-01-25 15:32:51 -0600

    Fix state after build error

--- frontend/src/components/Topbar.js
+++ frontend/src/components/Topbar.js
@@ -9,15 +9,13 @@ const Topbar = () => {
     return (
         <nav class="topbar">
             <div class="topbar-left-container">
-                <span>Algorithm Visualiser | A* Search</span>
+                <span>Visualise | A* Search</span>
             </div>
             <div class="topbar-right-container">
                 <div class="build-btn right-btn"><i class="fa-solid fa-wrench icon"></i>Build</div>
                 <div class="play-btn right-btn"><i class="fa-solid fa-play icon"></i>Play</div>
                 <div class="progress-bar-container">
-                    {/* <div class="step-btn">{"<"}</div> */}
                     <ProgressBar maxValue={maxValue} />
-                    {/* <div class="step-btn">{">"}</div> */}
                 </div>
                 <div class="speed-slider right-btn">Speed</div>
             </div>
