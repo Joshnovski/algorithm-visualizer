fb9d24d87a302f89dd70d0d8c4965a8486b9c88b
Joshnovski <josh.morphew@hotmail.com>
2024-01-25 17:34:52 -0600

    Fix speed slider

--- frontend/src/components/SpeedSlider.js
+++ frontend/src/components/SpeedSlider.js
@@ -3,9 +3,17 @@ import ReactSlider from 'react-slider';
 
 const SpeedSlider = () => {
     return (
-        <div class=" right-btn">
-            <div class="speed-slider">Speed</div>
-            <ReactSlider />
+        <div class=" right-btn speed-slider-container">
+            <div>Speed</div>
+            <ReactSlider
+                className="speed-slider"
+                thumbClassName="speed-slider-thumb"
+                trackClassName="speed-slider-track"
+                min={0}
+                max={2}
+                defaultValue={1}
+                // renderThumb={(props) => <div {...props}></div>}
+             />
         </div>
     );
 };
